c0d3ac12a490501a15475f3a21941d62449779a8
Pedro Orellana <pedro.orellana.manzano@gmail.com>
2019-01-23 19:47:32 -0600

    feat: added translation to all visible strings

--- weddingsite/snorlax/models.py
+++ weddingsite/snorlax/models.py
@@ -1,37 +1,38 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 
 class Person(models.Model):
-	full_name = models.CharField(max_length=100)
-	email = models.EmailField(max_length=254, blank=True)
+	full_name = models.CharField(max_length=100, verbose_name=_('full name'))
+	email = models.EmailField(max_length=254, blank=True, verbose_name=_('email'))
 
 	def __str__(self):
 		return self.full_name
 
 class FamilyGroup(models.Model):
-	created_date = models.DateTimeField(default=timezone.now)
+	created_date = models.DateTimeField(default=timezone.now, verbose_name=_('creation date'))
 	member = models.ManyToManyField(Person)
 
 	def __str__(self):
 		return str(self.id)
 
 RSVP_STATUS = (
-	('CONFIRMED', 'Si Asistire'),
-	('DECLINED', 'No Asistire')
+	('CONFIRMED', _('Accept')),
+	('DECLINED', _('Decline'))
 )
 
 MEAL_OPTIONS = (
-	('MEAL_1', 'Meal Option 1'),
-	('MEAL_2', 'Meal Option 2'),
-	('MEAL_3', 'Meal Option 3')
+	('MEAL_1', _('Meal Option 1')),
+	('MEAL_2', _('Meal Option 2')),
+	('MEAL_3', _('Meal Option 3'))
 )
 
 class RSVP(models.Model):
-	status = models.CharField(max_length=9, choices=RSVP_STATUS)
-	meal = models.CharField(max_length=20, choices=MEAL_OPTIONS)
-	notes = models.TextField(blank=True)
-	rsvp_date = models.DateTimeField(default=timezone.now)
+	status = models.CharField(max_length=9, choices=RSVP_STATUS, verbose_name=_('status'))
+	meal = models.CharField(max_length=20, choices=MEAL_OPTIONS, verbose_name=_('meal options'))
+	notes = models.TextField(blank=True, verbose_name=_('dietary restrictions'))
+	rsvp_date = models.DateTimeField(default=timezone.now, verbose_name='RSVP date')
 	person = models.OneToOneField(
 		Person,
 		on_delete=models.CASCADE,
