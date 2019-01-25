c755d7541ca33b24d71f44213ece2cba20f7547f
Pedro Orellana <pedro.orellana.manzano@gmail.com>
2019-01-24 22:54:39 -0600

    fix: set the correct language when redirecting from a 404

--- weddingsite/snorlax/views.py
+++ weddingsite/snorlax/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.utils.translation import get_language
 
 # Create your views here.
 def do_rsvp(request):
@@ -7,4 +8,5 @@ def do_rsvp(request):
 def view_404(request, exception):
     # make a redirect to homepage - in this case, to RSVP page
     # you can use the name of url or just the plain link
-    return redirect('/es/rsvp')
+    path = + '/' + get_language() + '/rsvp'
+    return redirect(path)
